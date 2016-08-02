0c96617dc6d8da2fcea1aa523da4a456fe379803
Matthew Costabile <mattcosta7@gmail.com>
2016-08-02 16:23:51 -0400

    heroku gem

--- app/controllers/departments_controller.rb
+++ app/controllers/departments_controller.rb
@@ -68,7 +68,7 @@ class DepartmentsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_department
-      @department = Department.find_by_slug(params[:id])
+      @department = Department.friendly.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
