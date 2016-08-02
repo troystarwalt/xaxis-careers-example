5865bf7eaebd274b007a55e4a33cb0a8c8694666
Matthew Costabile <mattcosta7@gmail.com>
2016-08-02 17:15:00 -0400

    chages default header sizing

--- app/uploaders/hero_image_uploader.rb
+++ app/uploaders/hero_image_uploader.rb
@@ -15,7 +15,7 @@ class HeroImageUploader < CarrierWave::Uploader::Base
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   def default_url
-    ActionController::Base.helpers.asset_path('hero-image.png')
+    "http://placehold.it/1600x900"
   end
 
   # Process files as they are uploaded:
