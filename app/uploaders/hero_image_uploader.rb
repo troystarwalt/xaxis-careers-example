53486c568f43470a7647cfb5d295431dd5875276
Matthew Costabile <mattcosta7@gmail.com>
2016-08-02 16:08:35 -0400

    fix heros

--- app/uploaders/hero_image_uploader.rb
+++ app/uploaders/hero_image_uploader.rb
@@ -15,7 +15,7 @@ class HeroImageUploader < CarrierWave::Uploader::Base
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   def default_url
-    '/assets/hero-image.png'
+    ActionController::Base.helpers.asset_path('hero-image.png')
   end
 
   # Process files as they are uploaded:
