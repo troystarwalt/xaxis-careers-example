4963aee0793871ae29f16d1c7942de1af97992df
Troy Starwalt <troy.starwalt@gmail.com>
2016-08-03 17:01:35 -0400

    updates issue10

--- app/assets/javascripts/layout.js
+++ app/assets/javascripts/layout.js
@@ -1,11 +1,21 @@
-// $(document).ready(function(){
-//   var $grid = $('.grid').masonry({
-//     itemSelector: '.grid-item',
-//     columnWidth: '.grid-sizer',
-//     percentPosition: true
-//   });
-//
-//   $grid.imagesLoaded().progress( function() {
-//     $grid.masonry('layout');
-//   });
-// })
+
+
+$(document).ready(function() {
+  $(window).load(function() {
+    var maxHeight = $('.vertical-img').height();
+    $('.tb-top').css({'height': maxHeight/2})
+  })
+  $(window).resize(function() {
+    var maxHeight = $('.vertical-img').height();
+    $('.tb-top').css({'height': maxHeight/2})
+  })
+  $(window).load(function() {
+    var maxHeight = $('.wide-img').height();
+    $('.tb-bottom').css({'height': maxHeight/2})
+  })
+  $(window).resize(function() {
+    var maxHeight = $('.wide-img').height();
+    $('.tb-bottom').css({'height': maxHeight/2})
+  })
+
+});
