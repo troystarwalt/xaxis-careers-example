b8f130a6b43830f53eebff1e2772998de634e1b7
Troy Starwalt <troy.starwalt@gmail.com>
2016-08-08 15:02:43 -0400

    added regions, departmetns and locations to search controller

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,10 +2,17 @@ class SearchesController < ApplicationController
 
   def index
     # @jobs = JobListing.all
-    if params[:search]
-      @jobs = JobListing.search(params[:search]).order("created_at DESC")
-    else
-      @jobs = JobListing.all.order('created_at DESC')
+    @title = "Xaxis Careers | " + params[:search]
+    @regions = Region.all.order(:id)
+    @departments = Department.all.order(:name)
+    @locations = Location.all.order(:name)
+    @jobs = Rails.cache.fetch('jobs',expires_in: 24.hour) do
+      JobListing.all
+    end
+      if params[:search]
+        @jobs = JobListing.search(params[:search]).order("created_at DESC")
+      else
+        @jobs = JobListing.all.order('created_at DESC')
+      end
     end
   end
-end
