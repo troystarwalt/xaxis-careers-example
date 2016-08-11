adb609f2b149c797b9ca5b17c93f612f18fd3cbe
Matthew Costabile <mattcosta7@gmail.com>
2016-08-11 15:51:45 -0400

    edits logout path for admin. fixes position of flashes

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -226,3 +226,10 @@ ActiveAdmin.setup do |config|
     end
   end
 end
+
+ActiveAdmin::Devise::SessionsController.class_eval do
+
+  def after_sign_out_path_for(resource_or_scope)
+    '/'
+  end
+end
