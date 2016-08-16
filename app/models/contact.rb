0f52fe2ef01d5fc8c8d7b9bd72cf4e21eaf0acdd
Troy Starwalt <troy.starwalt@gmail.com>
2016-08-16 12:08:54 -0400

    slight update to form and add sendgrid

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,14 +1,14 @@
 class Contact < MailForm::Base
   attribute :name, :validate => true
   attribute :email, :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
-  attribute :message
+  attribute :message, :validate => true
+  attribute :nickname, :captcha => true  # This is a spam blocker.
 
   def headers
     {
-      :subject => "Message from Xaxis Careers",
+      :subject => "Question from Xaxis Careers",
       :to => "troy.starwalt@xaxis.com",
       :from => %("#{name}" <#{email}>)
     }
   end
-
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,7 @@ Rails.application.configure do
   config.action_controller.perform_caching = true
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_deliveries = true
 
   # Print deprecation notices to the Rails logger.
@@ -43,13 +43,13 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.smtp_settings = {
-  address: "smtp.gmail.com",
+  address: "smtp.sendgrid.net",
   port: 587,
-  domain: 'gmail.com',
+  domain: 'www.xaxis-careers.com',
   authentication: "plain",
   enable_starttls_auto: true,
-  user_name: ENV["GMAIL_USERNAME"],
-  password: ENV["GMAIL_PASSWORD"]
+  user_name: ENV["SENDGRID_USERNAME"],
+  password: ENV["SENDGRID_PASSWORD"]
   }
 
   # Uncomment to try and debugg.
