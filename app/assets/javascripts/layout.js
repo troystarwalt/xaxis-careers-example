a5e4abc4943acd622f875cc6a8995e038e7182c0
Troy Starwalt <troy.starwalt@gmail.com>
2016-08-26 16:17:47 -0400

    updates js to add 50% height

--- app/assets/javascripts/layout.js
+++ app/assets/javascripts/layout.js
@@ -1,9 +1,15 @@
 $(document).ready(function() {
   var maxHeights = function() {
-    var maxHeightW = $('.wide-img').height();
-    var maxHeightV = $('.vertical-img').height();
-    $('.tb-bottom').css({'height': maxHeightV/2});
-    $('.tb-top').css({'height': maxHeightV/2});
+    if ( $(window).width() <= 675 ) {
+      var maxHeightW = $('.wide-img').height();
+      var maxHeightV = $('.vertical-img').height();
+      $('.tb-bottom').css({'height': maxHeightV/2});
+      $('.tb-top').css({'height': maxHeightV/2});
+    }
+    else {
+      $('.tb-bottom').css({'height': '50%'});
+      $('.tb-top').css({'height': '50%'});
+    }
   };
   $(window).load(function() {
     maxHeights();
